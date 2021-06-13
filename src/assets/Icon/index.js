4c58991ce8c503ebd8e2888cc360112c70ee390f
Dzun <nurroinz@gmail.com>
2021-06-13 07:05:29 +0700

    add icon assets

--- src/assets/Icon/index.js
+++ src/assets/Icon/index.js
@@ -5,11 +5,25 @@ import IcUserOff from './ic-user-off.svg';
 import IcGroupOn from './ic-group-on.svg';
 import IcGroupOff from './ic-group-off.svg';
 
+import IcDashUser from './ic-dash-users.png';
+import IcDashGroup from './ic-dash-gruops.png';
+import IcDashNas from './ic-dash-nas.png';
+import IcDashOpr from './ic-dash-opr.png';
+
+// import IcDashUser from './ic-dash-users.svg';
+// import IcDashGroup from './ic-dash-gruops.svg';
+// import IcDashNas from './ic-dash-nas.svg';
+// import IcDashOpr from './ic-dash-opr.svg';
+
 export {
     IcHomeOn,
     IcHomeOff,
     IcUserOn,
     IcUserOff,
     IcGroupOn,
-    IcGroupOff
+    IcGroupOff,
+    IcDashUser,
+    IcDashOpr,
+    IcDashGroup,
+    IcDashNas
 }
